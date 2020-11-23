0bfe07824dc51d472068343862ea5238cac04323
emobes22 <emilymobleybootcamp@gmail.com>
2020-11-23 09:38:57 -0500

    uploading js file

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { FAReactIconName } from "react-icons/fa";
+
+function App() {
+  return (
+    <div className="App">
+      <FaReact></FaReact>
+    </div>
+  );
+}
+
+export default App;
